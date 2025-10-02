fbd2fabc09921c07d38e5694a79101269e137543
TheSeventhStone <sgl1009@rit.edu>
2025-10-02 08:45:38 -0400

    Update Program.cs

--- Methods_And_Peer_Work/Methods_And_Peer_Work/Program.cs
+++ Methods_And_Peer_Work/Methods_And_Peer_Work/Program.cs
@@ -8,5 +8,9 @@
         //intialize
         samFavoriteNumber = 42;
         brandonFavoriteNumber = 13;
+
+        //start of our actual program
+        Console.WriteLine("Sam's favorite number is " + samFavoriteNumber);
+        Console.WriteLine("Brandon's favorite number is " + brandonFavoriteNumber);
     }
 }
