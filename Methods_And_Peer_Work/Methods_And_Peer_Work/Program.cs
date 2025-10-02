553f69c6567d3dcb530a783a627022b933e7b907
TheSeventhStone <sgl1009@rit.edu>
2025-10-02 08:43:08 -0400

    Update Program.cs

--- Methods_And_Peer_Work/Methods_And_Peer_Work/Program.cs
+++ Methods_And_Peer_Work/Methods_And_Peer_Work/Program.cs
@@ -3,8 +3,10 @@
     public static void Main(string[] args)
     {
         int samFavoriteNumber;
-        samFavoriteNumber = 42;
         int brandonFavoriteNumber;
+
+        //Intialization
+        samFavoriteNumber = 42;
         brandonFavoriteNumber = 13;
     }
 }
