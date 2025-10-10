96024dd7ac9cbe5a20dd8d6c50eb875e872f7ac4
TheSeventhStone <sgl1009@rit.edu>
2025-10-10 13:38:57 -0400

    fixed method classification for Subtract, auto commented Add

--- Methods_And_Peer_Work/Methods_And_Peer_Work/Program.cs
+++ Methods_And_Peer_Work/Methods_And_Peer_Work/Program.cs
@@ -2,6 +2,16 @@
 
 public class Program
 {
+    /*Samuel LaMere, Brandon Kass
+     IGME 201.01 - Interac. Des. & Alg. Prob. Sol. III
+    DUE: 10/12/2025
+    */
+    /// <summary>
+    /// Adds 2 given integer parameters
+    /// </summary>
+    /// <param name="samNumber1"></param>
+    /// <param name="brandonNumber1"></param>
+    /// <returns> samNumber1 + brandonNumber </returns>
     public static int Add(int samNumber1, int brandonNumber1)
     {
         return samNumber1 + brandonNumber1;
@@ -15,9 +25,9 @@ public class Program
         //intialize
         samFavoriteNumber = 42;
         brandonFavoriteNumber = 13;
-        answer = Add(42, 13);
+        answer = Add(samFavoriteNumber,brandonFavoriteNumber);
 
-        public int Subtract(int a, int b)
+        int Subtract(int a, int b)
         {
             return (a-b);
         }
@@ -25,6 +35,8 @@ public class Program
         Console.WriteLine("Sam's favorite number is " + samFavoriteNumber);
         Console.WriteLine("Brandon's favorite number is " + brandonFavoriteNumber);
         Console.WriteLine("When we add our favorite numbers together we get: " + answer);
+        answer = Subtract(samFavoriteNumber, brandonFavoriteNumber);
+        Console.WriteLine("When we subtract our favorite numbers from each other we get: " + answer);
 
 }
 }
